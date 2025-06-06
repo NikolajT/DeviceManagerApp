346a62ad1c4b5b9d0539c396956a0c27c09de817
NikolajT <nthom21@student.sdu.dk>
2025-06-07 00:08:21 +0200

    Razor page with button for testing

--- DeviceManagerApp/Program.cs
+++ DeviceManagerApp/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveWebAssemblyComponents();
+builder.Services.AddScoped<APIManager>();
+builder.Services.AddHttpClient();
 
 var app = builder.Build();
 
